841364e1015dcb9141161d0bf52acfa1bac633e7
Najla_abdulsalam <najlaa.abdulsalam@salvimar.gmbh>
2022-04-24 01:26:00 +0200

    init database

---
+++ Employees Managements/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Employees_Managements.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employees_Managements.Data
+    {
+    public class DataContext :DbContext
+        {
+        public DataContext(DbContextOptions options):base(options)
+            { 
+            }
+        
+     public DbSet<AppUser> Users { get; set; }
+        }
+    }
---
+++ Employees Managements/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Employees_Managements.Entities
+    {
+    public class AppUser
+        {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        }
+    }
